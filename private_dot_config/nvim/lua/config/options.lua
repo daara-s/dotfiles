96f35f653eb2ff19299fdcef75d976f6b677c390
Daara Shaw <daara@Daaras-MacBook-Air.local>
2023-10-24 08:46:47 +0100

    Fix clipboard conditional for windows in nvim

--- private_dot_config/nvim/lua/config/options.lua
+++ private_dot_config/nvim/lua/config/options.lua
@@ -3,19 +3,19 @@
 -- Add any additional options here
 
 -- Allows yanking to Windows clipboard from WSL
--- if vim.fn.has("wsl") then
---   vim.cmd([[
---   let g:clipboard = {
---     \   'name': 'WslClipboard',
---     \   'copy': {
---     \      '+': 'clip.exe',
---     \      '*': 'clip.exe',
---     \    },
---     \   'paste': {
---     \      '+': 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
---     \      '*': 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
---     \   },
---     \   'cache_enabled': 0,
---     \ }
---   ]])
--- end
+if vim.fn.has("wsl") == 1 then
+  vim.cmd([[
+  let g:clipboard = {
+    \   'name': 'WslClipboard',
+    \   'copy': {
+    \      '+': 'clip.exe',
+    \      '*': 'clip.exe',
+    \    },
+    \   'paste': {
+    \      '+': 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    \      '*': 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    \   },
+    \   'cache_enabled': 0,
+    \ }
+  ]])
+end
