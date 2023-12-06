551a953084163f9959ff3531de5aaf5cdf87991e
Daara Shaw <dev@daara.me>
2023-12-06 12:00:25 +0000

    add telescope ui for code actions

--- private_dot_config/nvim/lua/plugins/telescope.lua
+++ private_dot_config/nvim/lua/plugins/telescope.lua
@@ -1,5 +1,10 @@
 return {
-	'nvim-telescope/telescope.nvim',
-	tag = '0.1.5',
-	dependencies = { 'nvim-lua/plenary.nvim' }
+    {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.5',
+        dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    {
+        'nvim-telescope/telescope-ui-select.nvim'
+    },
 }
