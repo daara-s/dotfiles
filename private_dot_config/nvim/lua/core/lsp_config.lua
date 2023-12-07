5f152dc5bb9092584b07a4232b46ebeb0de23ffa
Daara Shaw <dev@daara.me>
2023-12-07 12:22:01 +0000

    add ignore line length to pylsp

--- private_dot_config/nvim/lua/core/lsp_config.lua
+++ private_dot_config/nvim/lua/core/lsp_config.lua
@@ -40,7 +40,7 @@ require('mason-lspconfig').setup({
                     pylsp = {
                         plugins= {
                             pycodestyle = {
-                                ignore = {'W391'},
+                                ignore = {'W391', 'E501'},
                             },
                             rope_autoimport = {
                                 enabled = true
@@ -97,22 +97,7 @@ require("telescope").setup {
   extensions = {
     ["ui-select"] = {
       require("telescope.themes").get_dropdown {
-        -- even more opts
       }
-
-      -- pseudo code / specification for writing custom displays, like the one
-      -- for "codeactions"
-      -- specific_opts = {
-      --   [kind] = {
-      --     make_indexed = function(items) -> indexed_items, width,
-      --     make_displayer = function(widths) -> displayer
-      --     make_display = function(displayer) -> function(e)
-      --     make_ordinal = function(e) -> string
-      --   },
-      --   -- for example to disable the custom builtin "codeactions" display
-      --      do the following
-      --   codeactions = false,
-      -- }
     }
   }
 }
