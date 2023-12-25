2471a9bb56643e8780fbccb8c174473c31263e56
Daara Shaw <dev@daara.me>
2023-12-25 14:05:47 +0000

    use telescope for lsp commands

--- private_dot_config/nvim/lua/core/lsp_config.lua
+++ private_dot_config/nvim/lua/core/lsp_config.lua
@@ -7,17 +7,22 @@ vim.api.nvim_create_autocmd('LspAttach', {
         local opts = {buffer = event.buf}
 
         vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-        vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+        vim.keymap.set('n', 'gK', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+        vim.keymap.set('n', 'gd', function() require("telescope.builtin").lsp_definitions({ reuse_win = true }) end,
+            { buffer = event.buf, desc = 'Goto Definition' })
         vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-        vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-        vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-        vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-        vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-        vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+        vim.keymap.set('n', 'gI', function() require("telescope.builtin").lsp_implementations({ reuse_win = true }) end,
+            { buffer = event.buf, desc = 'Goto Implementation' })
+        vim.keymap.set('n', 'gy', function() require("telescope.builtin").lsp_type_definitions({ reuse_win = true }) end,
+            { buffer = event.buf, desc = 'Goto T[y]pe Definition' })
+        vim.keymap.set('n', 'gr', '<cmd>Telescope lsp_references<cr>', opts)
+        vim.keymap.set('n', '<leader>cr', '<cmd>lua vim.lsp.buf.rename()<cr>',
+            { buffer = event.buf, desc = 'Rename' })
         vim.keymap.set({ 'n', 'x' }, '<leader>cf', function() vim.lsp.buf.format({ async = true }) end,
             { buffer = event.buf, desc = 'Format code' })
         vim.keymap.set('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<cr>',
             { buffer = event.buf, desc = 'Code action' })
+        vim.keymap.set('n', '<leader>cl', '<cmd>LspInfo<cr>', { desc = 'LSP info' })
 
         vim.keymap.set('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>', opts)
         vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>', opts)
