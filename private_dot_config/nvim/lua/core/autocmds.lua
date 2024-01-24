716ebeef7d86e3c5b1086d4f79e077eb40b9986d
Daara Shaw <dev@daara.me>
2024-01-24 21:10:50 +0000

    add auto resizing nvim window

--- private_dot_config/nvim/lua/core/autocmds.lua
+++ private_dot_config/nvim/lua/core/autocmds.lua
@@ -1,7 +1,19 @@
-vim.api.nvim_create_autocmd("TextYankPost",{
+vim.api.nvim_create_autocmd("TextYankPost", {
     group = vim.api.nvim_create_augroup("highlight_yank", {}),
     desc = "Highlight selection on yank",
     callback = function()
         vim.highlight.on_yank()
     end,
 })
+
+local wr_group = vim.api.nvim_create_augroup('WinResize', { clear = true })
+
+vim.api.nvim_create_autocmd(
+    'VimResized',
+    {
+        group = wr_group,
+        pattern = '*',
+        command = 'wincmd =',
+        desc = 'Automatically resize windows when the host window size changes.'
+    }
+)
