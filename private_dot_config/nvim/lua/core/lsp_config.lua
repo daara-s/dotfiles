3e4d088b9a0912f2a13f3df2ca3dddabb3edd613
Daara Shaw <dev@daara.me>
2024-02-02 22:51:30 +0000

    add searching config with telescope, minor nvim config clean up

--- private_dot_config/nvim/lua/core/lsp_config.lua
+++ private_dot_config/nvim/lua/core/lsp_config.lua
@@ -8,17 +8,21 @@ vim.api.nvim_create_autocmd('LspAttach', {
 
         vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
         vim.keymap.set('n', 'gK', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-        vim.keymap.set('n', 'gd', function() require("telescope.builtin").lsp_definitions({ reuse_win = true }) end,
+        vim.keymap.set('n', 'gd',
+            function() require("telescope.builtin").lsp_definitions({ reuse_win = true }) end,
             { buffer = event.buf, desc = 'Goto Definition' })
         vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-        vim.keymap.set('n', 'gI', function() require("telescope.builtin").lsp_implementations({ reuse_win = true }) end,
+        vim.keymap.set('n', 'gI',
+            function() require("telescope.builtin").lsp_implementations({ reuse_win = true }) end,
             { buffer = event.buf, desc = 'Goto Implementation' })
-        vim.keymap.set('n', 'gy', function() require("telescope.builtin").lsp_type_definitions({ reuse_win = true }) end,
+        vim.keymap.set('n', 'gy',
+            function() require("telescope.builtin").lsp_type_definitions({ reuse_win = true }) end,
             { buffer = event.buf, desc = 'Goto T[y]pe Definition' })
         vim.keymap.set('n', 'gr', '<cmd>Telescope lsp_references<cr>', opts)
         vim.keymap.set('n', '<leader>cr', '<cmd>lua vim.lsp.buf.rename()<cr>',
             { buffer = event.buf, desc = 'Rename' })
-        vim.keymap.set({ 'n', 'x' }, '<leader>cf', function() vim.lsp.buf.format({ async = true }) end,
+        vim.keymap.set({ 'n', 'x' }, '<leader>cf',
+            function() vim.lsp.buf.format({ async = true }) end,
             { buffer = event.buf, desc = 'Format code' })
         vim.keymap.set('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<cr>',
             { buffer = event.buf, desc = 'Code action' })
