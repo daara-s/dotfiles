f655ce35e817eb5b4e3bf85bead132f344bc8d5d
Daara Shaw <dev@daara.me>
2024-04-07 18:38:08 +0100

    update vim snippets

--- private_dot_config/nvim/lua/core/lsp_config.lua
+++ private_dot_config/nvim/lua/core/lsp_config.lua
@@ -4,7 +4,7 @@ local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
 vim.api.nvim_create_autocmd('LspAttach', {
     desc = 'LSP actions',
     callback = function(event)
-        local opts = {buffer = event.buf}
+        local opts = { buffer = event.buf }
 
         vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
         vim.keymap.set('n', 'gK', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
@@ -45,7 +45,7 @@ end
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-    ensure_installed = {"pyright", "ruff_lsp", "lua_ls"},
+    ensure_installed = { "pyright", "ruff_lsp", "lua_ls" },
     handlers = {
         default_setup,
         pyright = function()
@@ -67,7 +67,7 @@ require('mason-lspconfig').setup({
                             version = 'LuaJIT'
                         },
                         diagnostics = {
-                            globals = {'vim'},
+                            globals = { 'vim' },
                         },
                         workspace = {
                             library = {
@@ -81,29 +81,57 @@ require('mason-lspconfig').setup({
     },
 })
 
+local has_words_before = function()
+    unpack = unpack or table.unpack
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local luasnip = require("luasnip")
 local cmp = require('cmp')
 
 cmp.setup({
     sources = {
-        {name = 'nvim_lsp', max_item_count = 8, keyword_length = 2},
-        {name = 'buffer', max_item_count = 2},
-        -- should add {name='luasnip'},?
+        { name = 'nvim_lsp', max_item_count = 8, keyword_length = 2 },
+        { name = 'buffer',   max_item_count = 2 },
+        -- should add {name='luasnip'},
     },
-    mapping = cmp.mapping.preset.insert({
-        -- Enter key confirms completion item
+    mapping = {
         ['<CR>'] = cmp.mapping.confirm({ select = true }),
-        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-e>'] = cmp.mapping.abort(),
-        -- Ctrl + space triggers completion menu
         ['<C-Space>'] = cmp.mapping.complete(),
-    }),
+
+        ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+                -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+                -- that way you will only jump inside the snippet region
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            elseif has_words_before() then
+                cmp.complete()
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+    },
     snippet = {
         expand = function(args)
-            require('luasnip').lsp_expand(args.body)
+            luasnip.lsp_expand(args.body)
         end,
     },
 })
+require("luasnip.loaders.from_vscode").lazy_load()
 
 local telescope = require("telescope")
 -- This is your opts table
@@ -111,8 +139,8 @@ telescope.setup {
     defaults = {
         path_display = {
             shorten = {
-                len=1,
-                exclude={1, -1, -2}
+                len = 1,
+                exclude = { 1, -1, -2 }
             },
         },
     },
--- private_dot_config/nvim/lua/plugins/lsp.lua
+++ private_dot_config/nvim/lua/plugins/lsp.lua
@@ -1,8 +1,13 @@
 return {
-    {'neovim/nvim-lspconfig'},
-    {'williamboman/mason.nvim'},
-    {'williamboman/mason-lspconfig.nvim'},
-    {'hrsh7th/cmp-nvim-lsp'},
-    {'hrsh7th/nvim-cmp'},
-    {'L3MON4D3/LuaSnip'},
+    { 'neovim/nvim-lspconfig' },
+    { 'williamboman/mason.nvim' },
+    { 'williamboman/mason-lspconfig.nvim' },
+    { 'hrsh7th/cmp-nvim-lsp' },
+    { 'hrsh7th/nvim-cmp' },
+    {
+        'L3MON4D3/LuaSnip',
+        build = "make install_jsregexp"
+    },
+    { 'hrsh7th/cmp-buffer'},
+    { "rafamadriz/friendly-snippets" },
 }
