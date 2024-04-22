f595fc4e84789c494b34887e0d40bb2a5efd7ce7
Daara Shaw <d.shaw@draftkings.com>
2024-04-22 17:06:24 +0100

    move lsp config to plugins

--- private_dot_config/nvim/lua/plugins/lsp.lua
+++ private_dot_config/nvim/lua/plugins/lsp.lua
@@ -1,6 +1,60 @@
 return {
     { 'neovim/nvim-lspconfig' },
-    { 'williamboman/mason.nvim' },
-    { 'williamboman/mason-lspconfig.nvim' },
+    {
+        'williamboman/mason.nvim',
+        config = function()
+            require('mason').setup({})
+        end
+    },
+    {
+        'williamboman/mason-lspconfig.nvim',
+        config = function()
+            local lspconfig = require('lspconfig')
+            local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+            local default_setup = function(server)
+                lspconfig[server].setup({
+                    capabilities = lsp_capabilities,
+                })
+            end
+
+            require('mason-lspconfig').setup({
+                ensure_installed = { "pyright", "ruff_lsp", "lua_ls" },
+                handlers = {
+                    default_setup,
+                    pyright = function()
+                        require('lspconfig').pyright.setup({
+                            capabilities = lsp_capabilities,
+                            settings = {
+                                pyright = {
+                                    -- eg disableOrganiseImport = true,
+                                    typeCheckingMode = "off",
+                                }
+                            }
+                        })
+                    end,
+                    lua_ls = function()
+                        require('lspconfig').lua_ls.setup({
+                            settings = {
+                                Lua = {
+                                    runtime = {
+                                        version = 'LuaJIT'
+                                    },
+                                    diagnostics = {
+                                        globals = { 'vim' },
+                                    },
+                                    workspace = {
+                                        library = {
+                                            vim.env.VIMRUNTIME,
+                                        }
+                                    }
+                                }
+                            }
+                        })
+                    end,
+                },
+            })
+        end
+    },
     { 'hrsh7th/cmp-nvim-lsp' },
 }
