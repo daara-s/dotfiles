ddfa8239918beb9587310e7188a7f9c9c68e77a0
Daara Shaw <dev@daara.me>
2024-08-28 20:56:21 +0100

    add markview for markdown previewing

--- private_dot_config/nvim/lua/plugins/markdown.lua
+++ private_dot_config/nvim/lua/plugins/markdown.lua
@@ -1,6 +1,17 @@
 return {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = { "markdown" },
-    build = function() vim.fn["mkdp#util#install"]() end,
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        ft = { "markdown" },
+        build = function() vim.fn["mkdp#util#install"]() end,
+    },
+    {
+        "OXY2DEV/markview.nvim",
+        lazy = false, -- Recommended
+
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+            "nvim-tree/nvim-web-devicons"
+        }
+    }
 }
