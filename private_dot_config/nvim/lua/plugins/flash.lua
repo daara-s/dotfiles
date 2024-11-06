83f8b440856ba124cd8028f627cc6df4f2435c45
Daara Shaw <d.shaw@draftkings.com>
2024-11-06 16:57:18 +0000

    remove uppercase letters from flash pliugin

--- private_dot_config/nvim/lua/plugins/flash.lua
+++ private_dot_config/nvim/lua/plugins/flash.lua
@@ -2,7 +2,9 @@ return {
     "folke/flash.nvim",
     event = "VeryLazy",
     ---@type Flash.Config
-    opts = {},
+    opts = {
+        label = { uppercase = false, }
+    },
     -- stylua: ignore
     keys = {
         { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
