522d2197e9e96b636dd21c1fc517aefe3fa30310
Daara Shaw <d.shaw@draftkings.com>
2024-11-06 16:57:01 +0000

    remove nvim completions for cmdline

--- private_dot_config/nvim/lua/plugins/completion.lua
+++ private_dot_config/nvim/lua/plugins/completion.lua
@@ -56,23 +56,5 @@ return {
                 end,
             },
         })
-        -- `/` cmdline setup.
-        cmp.setup.cmdline('/', {
-            mapping = cmp.mapping.preset.cmdline(),
-            sources = {
-                { name = 'buffer' }
-            }
-        })
-
-        -- `:` cmdline setup.
-        cmp.setup.cmdline(':', {
-            mapping = cmp.mapping.preset.cmdline(),
-            sources = cmp.config.sources({
-                { name = 'path' }
-            }, {
-                { name = 'cmdline' }
-            }),
-            matching = { disallow_symbol_nonprefix_matching = false }
-        })
     end
 }
